562543fb78dee3e98aa374f57a39ada5b364974b
Bruno Schaatsbergen <git@bschaatsbergen.com>
2022-09-09 23:21:02 +0200

    Improve `overlaps` logging

--- cmd/overlaps.go
+++ cmd/overlaps.go
@@ -14,17 +14,20 @@ var overlapsCmd = &cobra.Command{
 	Short: "Checks if a CIDR range overlaps with another CIDR range",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 2 {
-			fmt.Println("Provide 2 cidr ranges")
+			fmt.Println("error: provide 2 CIDR ranges")
+			fmt.Println("See 'cidr overlaps -h' for help and examples")
 			os.Exit(1)
 		}
 		network1, err := core.ParseCIDR(args[0])
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("error: %s\n", err)
+			fmt.Println("See 'cidr overlaps -h' for help and examples")
 			os.Exit(1)
 		}
 		network2, err := core.ParseCIDR(args[1])
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("error: %s\n", err)
+			fmt.Println("See 'cidr overlaps -h' for help and examples")
 			os.Exit(1)
 		}
 		networksOverlap := overlaps(network1, network2)
