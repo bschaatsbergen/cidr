8993bd7ec2e71c02a99fcb1a458ae7d48c1b659a
Bruno Schaatsbergen <git@bschaatsbergen.com>
2022-09-09 23:18:58 +0200

    improve count logging

--- cmd/count.go
+++ cmd/count.go
@@ -15,12 +15,14 @@ var (
 		Short: "Return the count of distinct host addresses in a given CIDR range",
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) != 1 {
-				fmt.Println("Provide a cidr range")
+				fmt.Println("error: provide a CIDR range")
+				fmt.Println("See 'cidr count -h' for help and examples")
 				os.Exit(1)
 			}
 			network, err := core.ParseCIDR(args[0])
 			if err != nil {
-				fmt.Println("Provide a valid cidr range")
+				fmt.Printf("error: invalid CIDR range: %s\n", args[0])
+				fmt.Println("See 'cidr count -h' for help and examples")
 				os.Exit(1)
 			}
 			hostAddressCount := count(network)
